deed6d68dd4b5b2e4e686eea755ff3953c81a11f
Benjamin Lavian <benylavian@gmail.com>
2020-10-26 20:57:06 -0400

    added click functionality

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,1 +1,28 @@
-let currentPlayerSymbol = x;
+let currentPlayerSymbol = "x";
+let squareValues = ["", "", "", "", "", "", "", "", ""];
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  document
+    .getElementById("tic-tac-toe-board")
+    .addEventListener("click", (event) => {
+      const targetId = event.target.id;
+
+      if (!targetId.startsWith("square-")) return;
+
+      const squareIndex = Number.parseInt(targetId[targetId.length - 1]);
+
+      if (squareValues[squareIndex] !== "") return;
+
+      const img = document.createElement("img");
+      img.src = `https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-${currentPlayerSymbol}.svg`;
+      event.target.appendChild(img);
+
+      squareValues[squareIndex] = currentPlayerSymbol;
+
+      if (currentPlayerSymbol === "x") {
+        currentPlayerSymbol = "o";
+      } else {
+        currentPlayerSymbol = "x";
+      }
+    });
+});
